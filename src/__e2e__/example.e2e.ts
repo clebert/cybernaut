dbc141537d99cb56305aebdd5016b09278215da6
Clemens Akens <clebert@me.com>
2017-03-24 01:39:33 +0100

    test: remove example e2e test

--- src/__e2e__/example.e2e.ts
+++
@@ -1,7 +0,0 @@
-import {browser, it, test} from '..';
-
-test('Load "clebert/cybernaut" repository on GitHub', async t => {
-  await t.perform(browser.loadPage('https://github.com/clebert/cybernaut'));
-
-  await t.assert(browser.pageTitle, it.should.match(/clebert\/cybernaut/));
-});
