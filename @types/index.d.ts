d4e3a908d0dcb474e85666c29a4e4dc0b556c3c9
Clemens Akens <clebert@users.noreply.github.com>
2017-03-25 23:33:18 +0100

    fix: import all custom types as reference (#7)

---
+++ @types/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../@types/ajv.d.ts" />
+/// <reference path="../@types/deep-strict-equal.d.ts" />
+/// <reference path="../@types/glob.d.ts" />
+/// <reference path="../@types/selenium-webdriver.d.ts" />
+/// <reference path="../@types/sinon.d.ts" />
+/// <reference path="../@types/tap.d.ts" />
