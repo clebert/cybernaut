856f2507be0c4916404579a0adaa0ed2805d6b74
Clemens Akens <clebert@users.noreply.github.com>
2017-03-27 20:02:29 +0200

    docs: improve example (#14)

--- example/tests/cybernaut-repo.e2e.js
+++ example/tests/cybernaut-repo.e2e.js
@@ -1,7 +1,11 @@
-const {browser, it, test} = require('cybernaut');
+const {browser, defineElement, it, test} = require('cybernaut');
 
-test('Check existence of "clebert/cybernaut" repository on GitHub', async t => {
+test('Star repository "clebert/cybernaut" on GitHub', async t => {
   await t.perform(browser.loadPage('https://github.com/clebert/cybernaut'));
 
-  await t.assert(browser.pageTitle, it.should.match(/clebert\/cybernaut/));
+  await t.assert(browser.pageTitle, it.should.contain('clebert/cybernaut'));
+
+  const starButton = defineElement('ul.pagehead-actions > li:nth-child(2) > a:nth-child(1)');
+
+  await t.perform(starButton.click());
 });
