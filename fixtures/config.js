c4367874e765602f8afc9b7eb59deec335670f31
Clemens Akens <clebert@users.noreply.github.com>
2017-04-01 00:13:00 +0200

    test: add more tests for index.ts (closes #42) (#43)

---
+++ fixtures/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  capabilities: {browserName: 'phantomjs'},
+  concurrency: 2,
+  dependencies: [],
+  exclude: ['custom-config.js'],
+  include: '*.js',
+  retries: 3,
+  retryDelay: 1000
+};
--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -8,44 +8,70 @@ interface Result {
   readonly stderr: string;
 }
 
-let result: Result;
+const customStderr = `Config:
+  capabilities: { browserName: 'phantomjs' }
+  concurrency: 2
+  dependencies: []
+  exclude: [ 'custom-config.js' ]
+  include: '*.js'
+  retries: 3
+  retryDelay: 1000
+`;
+
+const defaultStderr = `Config:
+  capabilities: { browserName: 'chrome' }
+  concurrency: 1
+  dependencies: [ 'chromedriver' ]
+  exclude: [ '**/node_modules/**/*' ]
+  include: '**/*.e2e.js'
+  retries: 4
+  retryDelay: 500
+`;
+
+let customResult: Result;
+let defaultResult: Result;
 
 test.cb.before(t => {
-  execFile(process.execPath, [
-    join(__dirname, '../index.js'), 'test-config.js'
-  ], {cwd: join(__dirname, '../../fixtures')}, (error, stdout, stderr) => {
-    result = {error, stdout, stderr};
+  const bin = join(__dirname, '../index.js');
+  const cwd = join(__dirname, '../../fixtures');
+  const node = process.execPath;
 
-    t.end();
+  execFile(node, [bin, 'config.js'], {cwd}, (error, stdout, stderr) => {
+    customResult = {error, stdout, stderr};
+
+    // tslint:disable-next-line no-shadowed-variable
+    execFile(node, [bin], {cwd}, (error, stdout, stderr) => {
+      defaultResult = {error, stdout, stderr};
+
+      t.end();
+    });
   });
 });
 
 test('`error` should be null', t => {
-  t.plan(1);
+  t.plan(2);
 
-  t.is(result.error, null);
+  t.is(customResult.error, null);
+  t.is(defaultResult.error, null);
 });
 
 test('`stdout` should contain a test marked as TODO', t => {
-  t.plan(1);
+  t.plan(2);
 
-  t.regex(result.stdout, /ok 1 - foo # TODO/);
+  t.regex(customResult.stdout, /ok 1 - foo # TODO/);
+  t.regex(defaultResult.stdout, /ok 1 - foo # TODO/);
 });
 
 test('`stdout` should contain a test marked as SKIP', t => {
-  t.plan(1);
+  t.plan(2);
 
-  t.regex(result.stdout, /ok 2 - bar # SKIP/);
+  t.regex(customResult.stdout, /ok 2 - bar # SKIP/);
+  t.regex(defaultResult.stdout, /ok 2 - bar # SKIP/);
 });
 
 test('`stderr` should contain a copy of the configuration', t => {
-  t.plan(7);
+  t.plan(2);
 
-  t.regex(result.stderr, /capabilities: \{ browserName: 'chrome' \}/);
-  t.regex(result.stderr, /concurrency: 1/);
-  t.regex(result.stderr, /dependencies: \[\]/);
-  t.regex(result.stderr, /exclude: \[ '\*\*\/node_modules\/\*\*\/\*' \]/);
-  t.regex(result.stderr, /include: '\*\*\/\*\.e2e\.js'/);
-  t.regex(result.stderr, /retries: 4/);
-  t.regex(result.stderr, /retryDelay: 500/);
+  t.is(customResult.stderr.trim(), customStderr.trim());
+  t.is(defaultResult.stderr.trim(), defaultStderr.trim());
 });
