0f7f069f03b62d794e57457515df0bef370b54e7
Clemens Akens <clebert@users.noreply.github.com>
2017-04-04 15:37:10 +0200

    chore: rename folder `typings` to `types` (#67)

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../typings/ajv.d.ts" />
-/// <reference path="../typings/deep-strict-equal.d.ts" />
-/// <reference path="../typings/glob.d.ts" />
-/// <reference path="../typings/selenium-webdriver.d.ts" />
-/// <reference path="../typings/sinon.d.ts" />
-/// <reference path="../typings/tap.d.ts" />
-/// <reference path="../typings/uuid.d.ts" />
