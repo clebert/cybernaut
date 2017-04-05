b5696b390b17985eb0d332dc660dd320b6c80286
Clemens Akens <clebert@me.com>
2017-04-05 15:02:07 +0200

    test: move implementation type and execute into an own file

---
+++ src/implementation.ts
@@ -0,0 +1,44 @@
+import {Builder, WebDriver} from 'selenium-webdriver';
+import {Config} from './config';
+import {Test} from './test';
+
+export type Implementation = (t: Test) => Promise<void>;
+
+export type Options = Pick<
+  Config, 'capabilities' | 'retries' | 'retryDelay' | 'timeouts'
+  >;
+
+class TapTest extends Test {
+  private readonly tap: Tap.Test;
+
+  public constructor(driver: WebDriver, tap: Tap.Test, options: Options) {
+    super(driver, options.retries, options.retryDelay);
+
+    this.tap = tap;
+  }
+
+  public fail(message: string, cause: Error): void {
+    throw new Error(`${message} (cause: ${cause.message})`);
+  }
+
+  public pass(message: string): void {
+    this.tap.pass(message);
+  }
+}
+
+export async function execute(
+  implementation: Implementation, tap: Tap.Test, options: Options
+): Promise<void> {
+  const {capabilities, timeouts} = options;
+  const driver = await new Builder().withCapabilities(capabilities).build();
+
+  try {
+    await driver.manage().timeouts().implicitlyWait(timeouts.element);
+    await driver.manage().timeouts().pageLoadTimeout(timeouts.page);
+    await driver.manage().timeouts().setScriptTimeout(timeouts.script);
+
+    await implementation(new TapTest(driver, tap, options));
+  } finally {
+    await driver.quit();
+  }
+}
