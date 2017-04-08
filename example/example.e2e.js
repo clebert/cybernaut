2f9d23ebcf1517981ee884855cb07ddd433c0e55
Clemens Akens <clebert@me.com>
2017-04-09 01:16:10 +0200

    test: add an intentional error

--- example/example.e2e.js
+++ example/example.e2e.js
@@ -3,7 +3,7 @@ const {browser, defineElement, it, test} = require('cybernaut');
 test('Star the "clebert/cybernaut" repository on GitHub', async t => {
   await t.perform(browser.loadPage('https://github.com/clebert/cybernaut'));
 
-  await t.assert(browser.pageTitle, it.should.contain('clebert/cybernaut'));
+  await t.assert(browser.pageTitle, it.should.contain('clebert123/cybernaut'));
 
   await t.perform(browser.takeScreenshot());
 
