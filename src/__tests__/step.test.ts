d69fb96a2f35a7fd1702eaa07a2a8a78a353da8f
Clemens Akens <clebert@me.com>
2017-04-08 11:10:14 +0200

    test: remove test helper `deferred`

--- src/__tests__/step.test.ts
+++ src/__tests__/step.test.ts
@@ -2,7 +2,6 @@ import proxyquire = require('proxyquire');
 
 import test from 'ava';
 import {Stub, stub} from 'sinon';
-import {Deferred} from './deferred';
 
 const sleep = stub();
 
@@ -13,16 +12,11 @@ proxyquire('../step', {'./utils': {sleep}});
 
 import {run} from '../step';
 
-let deferred: Deferred;
 let step: Stub;
 
 test.beforeEach(() => {
   sleep.reset();
   sleep.resetBehavior();
-
-  deferred = new Deferred();
-
-  sleep.resolves(deferred);
 
   step = stub();
 
@@ -31,8 +25,8 @@ test.beforeEach(() => {
   step.onThirdCall().resolves(undefined);
 });
 
-test('\`run\` should return 1', async t => {
-  t.plan(4);
+test('`run` should return 1', async t => {
+  t.plan(3);
 
   step = stub().resolves(undefined);
 
@@ -40,21 +34,17 @@ test('\`run\` should return 1', async t => {
 
   t.is(attempts, 1);
 
-  t.false(deferred.done);
-
   t.is(sleep.callCount, 0);
 
   t.is(step.callCount, attempts);
 });
 
-test('\`run\` should return 3', async t => {
-  t.plan(6);
+test('`run` should return 3', async t => {
+  t.plan(5);
 
   const attempts = await run(step, 2, 50);
 
   t.is(attempts, 3);
-
-  t.true(deferred.done);
 
   t.is(sleep.callCount, 2);
   t.is(sleep.args[0][0], 50);
@@ -63,27 +53,36 @@ test('\`run\` should return 3', async t => {
   t.is(step.callCount, attempts);
 });
 
-test('\`run\` should throw an error with the message "foo"', async t => {
-  t.plan(4);
+test('`run` should throw an error with the message "foo"', async t => {
+  t.plan(3);
 
   await t.throws(run(step, 0, 50), 'foo');
-
-  t.false(deferred.done);
 
   t.is(sleep.callCount, 0);
 
   t.is(step.callCount, 1);
 });
 
-test('\`run\` should throw an error with the message "bar"', async t => {
-  t.plan(5);
+test('`run` should throw an error with the message "bar"', async t => {
+  t.plan(4);
 
   await t.throws(run(step, 1, 50), 'bar');
-
-  t.true(deferred.done);
 
   t.is(sleep.callCount, 1);
   t.is(sleep.args[0][0], 50);
 
   t.is(step.callCount, 2);
+});
+
+test('`run` should await `sleep`', async t => {
+  t.plan(4);
+
+  sleep.rejects(new Error('baz'));
+
+  await t.throws(run(step, 2, 50), 'baz');
+
+  t.is(sleep.callCount, 1);
+  t.is(sleep.args[0][0], 50);
+
+  t.is(step.callCount, 1);
 });
