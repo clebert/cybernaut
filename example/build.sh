b52701fb9ed542ae8aea658f3f04f51688c836f9
Clemens Akens <clebert@me.com>
2017-04-11 11:37:27 +0200

    chore: improve containers

--- example/build.sh
+++ example/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p screenshots && \
-docker build -t clebert/cybernaut-example . && \
-docker run -ti --rm -v $(cd screenshots; pwd):/opt/e2e-test/screenshots clebert/cybernaut-example
+DIRNAME=`pwd`/`dirname $0`
+
+docker build -t clebert/cybernaut-example "$DIRNAME"
