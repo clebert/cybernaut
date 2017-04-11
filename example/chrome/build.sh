45eefde5a32ce12830bcf3805b12b027007dd19e
Clemens Akens <clebert@me.com>
2017-04-11 11:58:22 +0200

    chore: add example

---
+++ example/chrome/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIRNAME=`pwd`/`dirname $0`
+
+cp -f "$DIRNAME/../example.e2e.js" "$DIRNAME/example.e2e.js" && \
+
+docker build -t clebert/cybernaut-example-chrome "$DIRNAME"
+
+rm -f "$DIRNAME/example.e2e.js"
