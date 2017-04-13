96be6de50070af1d4b022745963eb8b79b94be56
Clemens Akens <clebert@me.com>
2017-04-13 19:04:42 +0200

    fix: add versioning for the docker containers

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  RESULT=$(CI= $(npm bin)/semantic-release pre 2>&1 >/dev/null | grep 'Determined version ' | cut -d '{' -f 2 | cut -d '}' -f 1)
-  VERSION=$(node -p "o={${RESULT}};o.version||''")
-
-  if [ -z "$VERSION" ]; then
-    echo 'There are no relevant changes, so no new version is released.'
-    exit 0
-  fi
-
-  echo "Determined version $VERSION"
-
   if [ -z "$DOCKER_USERNAME" ]; then
     echo 'Missing environment variable: DOCKER_USERNAME'
     exit 1
@@ -25,6 +15,16 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
     echo 'Illegal argument: target'
     exit 1
   fi
+
+  RESULT=$(CI= $(npm bin)/semantic-release pre 2>&1 >/dev/null | grep 'Determined version ' | cut -d '{' -f 2 | cut -d '}' -f 1)
+  VERSION=$(node -p "o={${RESULT}};o.version||''")
+
+  if [ -z "$VERSION" ]; then
+    echo 'There are no relevant changes, so no new version is released.'
+    exit 0
+  fi
+
+  echo "Determined version $VERSION" && \
 
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" && \
   docker push "clebert/cybernaut-$1:latest" && \
