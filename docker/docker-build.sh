fea405f3b8459751bff3a93764c13def11315ad6
Clemens Akens <clebert@users.noreply.github.com>
2017-04-13 20:04:49 +0200

    fix: add versioning for the docker containers (#108)

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -5,6 +5,9 @@ if [ "$1" != "chrome" ] && [ "$1" != "firefox" ]; then
   exit 1
 fi
 
+RESULT=$(CI= $(npm bin)/semantic-release pre 2>&1 >/dev/null | grep 'Determined version ' | cut -d '{' -f 2 | cut -d '}' -f 1)
+VERSION=$(node -p "o={${RESULT}};o.version||''")
+
 mkdir -p "docker/cybernaut-$1/cybernaut" && \
 
 cp -f config-schema.json "docker/cybernaut-$1/cybernaut/config-schema.json" && \
@@ -13,7 +16,13 @@ cp -f package.json "docker/cybernaut-$1/cybernaut/package.json" && \
 
 cp -f docker/cybernaut-xvfb "docker/cybernaut-$1/cybernaut-xvfb" && \
 
-docker build -t "clebert/cybernaut-$1" "docker/cybernaut-$1"
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ -n "$VERSION" ]; then
+  echo "Determined version $VERSION" && \
+
+  docker build -t "clebert/cybernaut-$1:latest" -t "clebert/cybernaut-$1:$VERSION" "docker/cybernaut-$1"
+else
+  docker build -t "clebert/cybernaut-$1:latest" "docker/cybernaut-$1"
+fi
 
 STATUS=$?
 
