35c5d00555ec8bda63d3eb1581677b24fb14fdc4
Clemens Akens <clebert@me.com>
2017-05-15 10:50:30 +0200

    docs: update example

--- README.md
+++ README.md
@@ -19,9 +19,11 @@ import {browser, defineElement, it, test} from 'cybernaut';
 test('The gitbook should include the chapter "Starting Cybernaut"', async t => {
   await t.perform(browser.loadPage('https://cybernaut.js.org/'), {retries: 0});
 
-  const summary = defineElement('summary', 'div.book-summary');
+  const chapterLink = defineElement(
+    'chapter-link-1-2', 'li[data-level="1.2"].chapter'
+  );
 
-  if (await t.verify(summary.visibility, it.should.equal(false))) {
+  if (await t.verify(chapterLink.visibility, it.should.equal(false))) {
     const toggleSummaryButton = defineElement(
       'toggle-summary-button', 'a:nth-child(1).js-toolbar-action'
     );
@@ -30,10 +32,6 @@ test('The gitbook should include the chapter "Starting Cybernaut"', async t => {
 
     await t.perform(browser.sleep(1000, 'an animation is running'));
   }
-
-  const chapterLink = defineElement(
-    'chapter-link-1-2', 'li[data-level="1.2"].chapter'
-  );
 
   await t.perform(chapterLink.click());
 
--- example/example.e2e.ts
+++ example/example.e2e.ts
@@ -4,9 +4,19 @@ import {browser, defineElement, it, test} from 'cybernaut';
 test('The gitbook should include the chapter "Starting Cybernaut"', async t => {
   await t.perform(browser.loadPage('https://cybernaut.js.org/'), {retries: 0});
 
-  const summary = defineElement('summary', 'div.book-summary');
+  const bookBody = defineElement('book-body', '.book-body');
 
-  if (await t.verify(summary.visibility, it.should.equal(false))) {
+  if (!(await t.verify(
+    bookBody.cssValue('position'), it.should.equal('absolute')
+  ))) {
+    await t.perform(browser.reloadPage(), {retries: 0});
+  }
+
+  const chapterLink = defineElement(
+    'chapter-link-1-2', 'li[data-level="1.2"].chapter'
+  );
+
+  if (!(await t.verify(chapterLink.visibility, it.should.equal(true)))) {
     const toggleSummaryButton = defineElement(
       'toggle-summary-button', 'a:nth-child(1).js-toolbar-action'
     );
@@ -15,10 +25,6 @@ test('The gitbook should include the chapter "Starting Cybernaut"', async t => {
 
     await t.perform(browser.sleep(1000, 'an animation is running'));
   }
-
-  const chapterLink = defineElement(
-    'chapter-link-1-2', 'li[data-level="1.2"].chapter'
-  );
 
   await t.perform(chapterLink.click());
 
