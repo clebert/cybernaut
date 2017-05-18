453c431d6912ea511fdd8b28f94715071ce53343
Clemens Akens <clebert@me.com>
2017-05-19 00:00:34 +0200

    refactor: reduce referenced types

--- types/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../types/ajv.d.ts" />
-/// <reference path="../types/debug.d.ts" />
-/// <reference path="../types/deep-strict-equal.d.ts" />
-/// <reference path="../types/globby.d.ts" />
-/// <reference path="../types/selenium-webdriver.d.ts" />
-/// <reference path="../types/tap.d.ts" />
