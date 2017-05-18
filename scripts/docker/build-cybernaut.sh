86a1a83f963544c90a8ed01dabb71d54ca72a658
Clemens Akens <clebert@users.noreply.github.com>
2017-05-18 20:47:03 +0200

    chore(package): remove semantic-release (#210)

--- scripts/docker/build-cybernaut.sh
+++ scripts/docker/build-cybernaut.sh
@@ -7,9 +7,6 @@ if [ "$1" != "chrome" ] && [ "$1" != "firefox" ]; then
   exit 1
 fi
 
-RESULT=$(CI='' "$(npm bin)"/semantic-release pre 2>&1 >/dev/null | grep 'Determined version ' | cut -d '{' -f 2 | cut -d '}' -f 1)
-VERSION=$(node -p "o={${RESULT}};o.version||''")
-
 mkdir -p "docker/cybernaut-$1/cybernaut"
 
 cp -f config-schema.json "docker/cybernaut-$1/cybernaut/config-schema.json"
@@ -19,13 +16,8 @@ cp -rf types "docker/cybernaut-$1/cybernaut/types"
 
 cp -f scripts/lib/cybernaut-xvfb.sh "docker/cybernaut-$1/cybernaut-xvfb.sh"
 
-if [ -n "$VERSION" ]; then
-  echo "Determined version $VERSION"
-
-  docker build -t "clebert/cybernaut-$1:latest" -t "clebert/cybernaut-$1:$VERSION" "docker/cybernaut-$1"
-else
-  docker build -t "clebert/cybernaut-$1:latest" "docker/cybernaut-$1"
-fi
+# docker build -t "clebert/cybernaut-$1:latest" -t "clebert/cybernaut-$1:$VERSION" "docker/cybernaut-$1"
+docker build -t "clebert/cybernaut-$1:latest" "docker/cybernaut-$1"
 
 rm -rf "docker/cybernaut-$1/cybernaut"
 rm -f "docker/cybernaut-$1/cybernaut-xvfb.sh"
