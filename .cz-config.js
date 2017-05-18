e17bc10b466f9ca536368c80955509ee7b772a9a
Clemens Akens <clebert@users.noreply.github.com>
2017-05-18 21:04:54 +0200

    chore: improve package files (#208)

--- .cz-config.js
+++ .cz-config.js
@@ -3,11 +3,7 @@
 module.exports = {
   allowBreakingChanges: ['feat'],
   allowCustomScopes: false,
-  scopes: [
-    {name: 'docker'},
-    {name: 'example'},
-    {name: 'package'}
-  ],
+  scopes: [''],
   types: [
     {value: 'feat',     name: 'feat:     A new feature'},
     {value: 'fix',      name: 'fix:      A bug fix'},
